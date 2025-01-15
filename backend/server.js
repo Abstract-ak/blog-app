8227c67f6f2758706d3163311951975bf140656f
Abstract-ak <ash.king0205@gmail.com>
2025-01-15 16:49:23 +0530

    initiate routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const connectDB = require("./src/config/db");
+const blogRoutes = require("./src/routes/blogRoutes");
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ const app = express();
 app.use(express.json());
 
 //  Routes
+app.use("/api/blogs", blogRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
