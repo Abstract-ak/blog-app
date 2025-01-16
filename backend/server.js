d3efed6c0c77edf102c76aa0bc2842b3b24e6003
Abstract-ak <ash.king0205@gmail.com>
2025-01-16 11:25:23 +0530

    error handle api

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const connectDB = require("./src/config/db");
 const blogRoutes = require("./src/routes/blogRoutes");
+const { errorHandler } = require("./src/middleware/errorHandler");
 
 dotenv.config();
 
@@ -17,6 +18,9 @@ app.use(express.json());
 
 //  Routes
 app.use("/api/blogs", blogRoutes);
+
+//  Error handling middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
